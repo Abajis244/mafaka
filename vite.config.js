c6cbdf7c73c2b6e055ac2a8eb29ea2f7f21e435f
Abajis244 <saeedajibolaabdulrahman24434@gmail.com>
2026-09-21 10:17:17 +0100

    Fix asset paths with relative base

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // The dot makes all asset paths relative so it works everywhere!
 })
